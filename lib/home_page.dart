be7d361d2ace023c4dd4950e463d16ed13ad0c53
Saddam <sddmhossain@786gmail.com>
2024-05-05 23:14:56 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Practice'),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('WelCome to flutter'),
+          ],
+        ),
+      ),
+    );
+  }
+}
