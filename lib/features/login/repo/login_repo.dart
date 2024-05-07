86ac0e4d515abd3a1214042e0b66d57212545541
Saddam <sddmhossain@786gmail.com>
2024-05-07 10:18:24 +0530

    code updated

--- lib/features/login/repo/login_repo.dart
+++ lib/features/login/repo/login_repo.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_demo/features/login/model/login_model.dart';
+import 'package:flutter/material.dart';
 
 class LoginRepo {
   // METHOD WITHOUT MODEL CLASS
@@ -7,6 +8,7 @@ class LoginRepo {
   Future<UserCredential> login({required LoginModel loginModel}) async {
     // ALWAYS USE TRY-CATCH
     try {
+      debugPrint('Logging with: ${loginModel.email}, ${loginModel.password}');
       UserCredential credential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(
               email: loginModel.email, password: loginModel.password);
--- lib/features/login/screen/login_screen.dart
+++ lib/features/login/screen/login_screen.dart
@@ -21,70 +21,99 @@ class _LoginScreenState extends State<LoginScreen> {
     log('PAGES REFRESHED');
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // EMAIL
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter Your Email id',
-                ),
-              ),
-              const SizedBox(height: 20),
-              // PASSWORD
-              TextFormField(
-                controller: _psswordController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter Your Password',
-                ),
-              ),
-              const SizedBox(height: 20),
-              // LOGIN BUTTON
-              _isLoading == true
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : ElevatedButton(
-                      onPressed: () async {
-                        if (_emailController.text.isNotEmpty &&
-                            _psswordController.text.isNotEmpty) {
-                          // LOGIN LOGIC
-                          log(_emailController.text.toString());
-                          log(_psswordController.text.toString());
-                          setState(() {
-                            _isLoading = true;
-                          });
-                          await Future.delayed(const Duration(seconds: 2));
-                          LoginRepo loginRepo = LoginRepo();
-
-                          UserCredential userCredential = await loginRepo.login(
-                            loginModel: LoginModel(
-                              email: _emailController.text.toLowerCase().trim(),
-                              password: _psswordController.text.trim(),
-                            ),
-                          );
-                          setState(() {
-                            _isLoading = false;
-                          });
-                          log(userCredential.toString());
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content:
-                                  Text('Please provide email and password'),
-                              backgroundColor: Colors.redAccent,
-                            ),
-                          );
-                        }
-                      },
-                      child: const Text('Login'),
-                    ),
+          child: Container(
+        decoration: const BoxDecoration(
+          gradient: SweepGradient(
+            tileMode: TileMode.mirror,
+            colors: [
+              Colors.indigo,
+              Colors.white54,
+              Colors.deepOrangeAccent,
             ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                // EMAIL
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter Your Email id',
+                      counterStyle: TextStyle(
+                        fontSize: 28,
+                        color: Colors.black,
+                      )),
+                  style: const TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // PASSWORD
+                TextFormField(
+                  controller: _psswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Your Password',
+                    counterStyle: TextStyle(
+                      fontSize: 28,
+                    ),
+                  ),
+                  style: const TextStyle(
+                    fontSize: 28,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // LOGIN BUTTON
+                _isLoading == true
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ElevatedButton(
+                        onPressed: () async {
+                          if (_emailController.text.isNotEmpty &&
+                              _psswordController.text.isNotEmpty) {
+                            // LOGIN LOGIC
+                            log(_emailController.text.toString());
+                            log(_psswordController.text.toString());
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            await Future.delayed(const Duration(seconds: 2));
+                            LoginRepo loginRepo = LoginRepo();
+
+                            UserCredential userCredential =
+                                await loginRepo.login(
+                              loginModel: LoginModel(
+                                email:
+                                    _emailController.text.toLowerCase().trim(),
+                                password: _psswordController.text.trim(),
+                              ),
+                            );
+                            setState(() {
+                              _isLoading = false;
+                            });
+                            log(userCredential.toString());
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content:
+                                    Text('Please provide email and password'),
+                                backgroundColor: Colors.redAccent,
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text('Login'),
+                      ),
+              ],
+            ),
           ),
         ),
       )),
